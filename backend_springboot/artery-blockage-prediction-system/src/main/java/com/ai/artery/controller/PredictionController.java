8e3135900b41cd599f662c758f2d0469cb4b0d8d
Sakshi_Shirude <Shirudesakshi05@gmail.com>
2026-05-11 22:29:14 +0530

    Developed prediction REST controller

---
+++ backend_springboot/artery-blockage-prediction-system/src/main/java/com/ai/artery/controller/PredictionController.java
@@ -0,0 +1,29 @@
+package com.ai.artery.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ai.artery.dto.PredictionRequest;
+import com.ai.artery.dto.PredictionResponse;
+import com.ai.artery.service.PredictionService;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("*")
+public class PredictionController {
+	
+	@Autowired
+	private PredictionService predictionService;
+	
+	@PostMapping("/predict")
+	public PredictionResponse predictHeartDiesease(
+			@RequestBody PredictionRequest request) {
+		
+		return predictionService.predictHeartDisease(request);
+	}
+
+}
