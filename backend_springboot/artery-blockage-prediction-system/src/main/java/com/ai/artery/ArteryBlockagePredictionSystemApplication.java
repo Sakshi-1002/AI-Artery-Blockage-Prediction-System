5b4614d6af0f8140c6b17929f302be79ac91867f
Sakshi_Shirude <Shirudesakshi05@gmail.com>
2026-05-11 22:47:04 +0530

    Tested Spring Boot and Flask integration

--- backend_springboot/artery-blockage-prediction-system/src/main/java/com/ai/artery/ArteryBlockagePredictionSystemApplication.java
+++ backend_springboot/artery-blockage-prediction-system/src/main/java/com/ai/artery/ArteryBlockagePredictionSystemApplication.java
@@ -2,8 +2,9 @@ package com.ai.artery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ArteryBlockagePredictionSystemApplication {
 
 	public static void main(String[] args) {
